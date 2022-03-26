6a1bc5877c20612532f9d4812cfe0187f37106bd
Kolya2019 <59363261+Kolya2019@users.noreply.github.com>
2022-03-26 15:35:03 +0200

    second

--- User.php
+++ User.php
@@ -1,1 +1,19 @@
-<??>
+<?
+$server = 'localhost';
+$user = 'root';
+$password = '';
+
+$dblink = mysqli_connect($server, $user, $password);
+
+if($dblink)
+echo 'Соединение установлено.';
+else
+die('Ошибка подключения к серверу баз данных.');
+
+$database = 'test';
+$selected = mysqli_select_db($database, $dblink);
+if($selected)
+echo ' Подключение к базе данных прошло успешно.';
+else
+die(' База данных не найдена или отсутствует доступ.');
+?>
